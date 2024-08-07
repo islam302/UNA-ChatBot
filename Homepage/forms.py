e1880daa80905d6aa2d68686cfcf9c1e15e5f4f9
islam302 <islambadran39@gmail.com>
2024-08-07 07:46:42 +0300

    update edit_question form

--- Homepage/forms.py
+++ Homepage/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from .models import QuestionAnswer
 
+
 class AddQuestionForm(forms.ModelForm):
     class Meta:
         model = QuestionAnswer
         fields = ['question', 'answer']
+        widgets = {
+            'question': forms.TextInput(attrs={'class': 'form-control'}),
+            'answer': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
 
 class QuestionForm(forms.Form):
     question = forms.CharField(label='Your question', max_length=255)
