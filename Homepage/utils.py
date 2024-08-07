e6d4902ba9f51f5f3f13f2aff0ac683b01c7387f
islam302 <islambadran39@gmail.com>
2024-08-07 04:08:31 +0300

    Add en_core_web_md

--- Homepage/utils.py
+++ Homepage/utils.py
@@ -7,37 +7,6 @@ import spacy
 nlp = spacy.load('en_core_web_md')
 
 
-# def save_excel_to_db(uploaded_file):
-#     wb = openpyxl.load_workbook(uploaded_file)
-#     sheet = wb.active
-#
-#     header = {cell.value: idx for idx, cell in enumerate(sheet[1])}
-#     question_col = header.get('question')
-#     answer_col = header.get('answer')
-#
-#     if question_col is None or answer_col is None:
-#         raise ValueError("Excel file must contain 'question' and 'answer' columns")
-#
-#     processed_data = []
-#     for row in sheet.iter_rows(min_row=2, values_only=True):
-#         question = row[question_col]
-#         answer = row[answer_col]
-#         if question and answer:
-#             # check if the question already exists
-#             existing_entry = QuestionAnswer.objects.filter(question=question).first()
-#             if existing_entry:
-#                 # Update the answer if the question exists
-#                 existing_entry.answer = answer
-#                 existing_entry.save()
-#                 processed_data.append({'question': question, 'answer': answer, 'updated': True})
-#             else:
-#                 # Create a new entry if the question doesn't exist
-#                 question_answer = QuestionAnswer(question=question, answer=answer)
-#                 question_answer.save()
-#                 processed_data.append({'question': question, 'answer': answer, 'updated': False})
-#     wb.close()
-#     return processed_data
-
 def save_excel_to_db(uploaded_file):
     wb = openpyxl.load_workbook(uploaded_file)
     sheet = wb.active
